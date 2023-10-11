275986ba004c9555aa2b1299711708061cab8df3
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-11 11:09:33 +0300

    Initial commit

---
+++ src/main/java/by/flameksandr/covidinformationsystem/CovidInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package by.flameksandr.covidinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidInformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidInformationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/flameksandr/covidinformationsystem/CovidInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.covidinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidInformationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
